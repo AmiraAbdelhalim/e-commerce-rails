945530aabc12ed02bfb543aa69d6ff398218e8fd
Tarek-Hassan <tarek446240@gmail.com>
2020-05-09 00:02:14 +0200

    merge

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,35 +1,40 @@
 
 ActiveAdmin.register Product do
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-    permit_params :title, :description, :price, :in_stock, :category_id, :brand_id,:user_id, images: []
+  permit_params :title, :description, :price, :in_stock, :category_id, :brand_id,:admin_user_id, images: []
   
   form do |f|
     f.semantic_errors # shows errors on :base
-    # f.inputs          # builds an input field for every attribute
     f.inputs do
       f.input :category_id, as: :select, collection: Category.all
       f.input :brand_id, as: :select, collection: Brand.all
+      # f.input :admin_user_id, as: :select, collection: AdminUser.all
       f.input :admin_user_id, as: :select, collection: AdminUser.where(is_seller: true)
-      # f.input :user_id, as: :select, collection: User.all
       f.input :title
       f.input :description
       f.input :price
       f.input :in_stock
       f.input :images, as: :file,input_html: {multiple: true}
-      # f.input :images, as: :file,input_html: {multiple: true}
     end
     f.actions         # adds the 'Submit' and 'Cancel' buttons
   end
+# controller do
+#   def create
 
+#     render plain: params[:current_admin_user].inspect
+#     # @product=Product.new(permit_params.merge(user_id:current_user.id))
+#   #   @product=Product.new(permitted_params.merge(user_id:current_user.id))
+
+#   #   if @product.save
+#   #           redirect_to @product
+#   #       else
+#   #           render 'new'
+#   #       end
+#   end
+# end
   # show page
   show do
     countImg=0;
-    # attributes_table do
     attributes_table  do 
       
       row :title
@@ -49,5 +54,4 @@ ActiveAdmin.register Product do
     end
     active_admin_comments
   end
-  # http://localhost:3000/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBEUT09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--86c356668fd5f6c80a71db7f4cdeea5bf4814867/images.jpeg
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,9 @@ class Product < ApplicationRecord
   belongs_to :category
   belongs_to :brand
   belongs_to :admin_user
-  validates :title,presence: true,format: { with: /\A[a-zA-Z]+\z/,
-  message: "Product Title Should Be Letters and Not NULL" }
+  validates :title,presence: true
+  # format: { with: /\A[a-zA-Z]+\z/,
+  # message: "Product Title Should Be Letters and Not NULL" }
   validates :description,presence: true
   validates :price,presence: true
   validates :in_stock,presence: true

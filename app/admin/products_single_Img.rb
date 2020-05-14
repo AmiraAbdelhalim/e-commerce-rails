6910b31df810f46d26ae5a1614de53d48031b96a
attia <mohamedahmed4362gmail.com>
2020-05-14 21:18:25 +0200

    finall

--- app/admin/products_single_Img.rb
+++ app/admin/products_single_Img.rb
@@ -5,13 +5,13 @@ ActiveAdmin.register Product do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :title, :description, :price, :in_stock, :category_id, :brand_id,:user_id, :image
+  permit_params :title, :description, :price, :in_stock, :category_id, :brand_id,:user_id, :images
   
   form do |f|
     f.semantic_errors # shows errors on :base
     f.inputs          # builds an input field for every attribute
     f.inputs do 
-      f.input :image, as: :file
+      f.input :images, as: :file
       # f.input :images, as: :file,input_html: {multiple: true}
     end
     f.actions         # adds the 'Submit' and 'Cancel' buttons
@@ -26,8 +26,8 @@ ActiveAdmin.register Product do
       row :in_stock
       row :category_id
       row :brand_id
-      row :image do |ad|
-        image_tag ad.image
+      row :images do |ad|
+        image_tag ad.images
       end
     end
     active_admin_comments
